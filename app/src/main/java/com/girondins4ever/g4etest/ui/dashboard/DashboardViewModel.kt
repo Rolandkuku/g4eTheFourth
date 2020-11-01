02d1e569b352b47bff17eeedca0274e47b343aa8
Grollec <renaud.bellec.3@gmail.com>
2020-11-01 10:39:54 +0100

    init app

---
+++ app/src/main/java/com/girondins4ever/g4etest/ui/dashboard/DashboardViewModel.kt
@@ -0,0 +1,13 @@
+package com.girondins4ever.g4etest.ui.dashboard
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class DashboardViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is dashboard Fragment"
+    }
+    val text: LiveData<String> = _text
+}
---
+++ app/src/main/java/com/girondins4ever/g4etest/ui/home/HomeViewModel.kt
@@ -0,0 +1,13 @@
+package com.girondins4ever.g4etest.ui.home
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is home Fragment"
+    }
+    val text: LiveData<String> = _text
+}
